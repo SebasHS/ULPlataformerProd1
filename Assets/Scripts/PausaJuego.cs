9bfb52793c7ca8536301260922843b4591f15846
angelotacoj <angelotacoj@gmail.com>
2023-09-16 20:29:04 -0500

    Pregunta 4 - UI Game Paused Canvas
    Add: Canvas + 2 Buttons + Logic
    Co-Authored-By: SebasHS <103210018+SebasHS@users.noreply.github.com>

---
+++ Assets/Scripts/PausaJuego.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausaJuego : MonoBehaviour
+{
+
+    private bool isPaused = false;
+    public GameObject pausePanel;
+
+    void Start()
+    {
+        pausePanel.SetActive(false); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; 
+        pausePanel.SetActive(true); 
+    }
+
+    void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; 
+        pausePanel.SetActive(false); 
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        ResumeGame();
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
